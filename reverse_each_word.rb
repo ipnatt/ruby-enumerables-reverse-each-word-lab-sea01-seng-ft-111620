8af7e0d4b93a67dde3e70949d8a6e121bc37998e
Irvin Natt <ipnatt@gmail.com>
2020-11-09 00:40:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
-  array = string.split(" ")
-  array.collect do |word|
+  split_string = string.split(" ")
+  split_string.collect do |word|
     word.reverse!
   end
-  array.join(" ")
+  split_string.join(" ")
 end
